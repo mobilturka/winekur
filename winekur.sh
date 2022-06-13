7f7c78d477f6cab217cbf1639cda2e7281610faf
mt <93243536+mobilturka@users.noreply.github.com>
2022-06-13 13:25:21 +0300

    Update winekur.sh

--- winekur.sh
+++ winekur.sh
@@ -8,7 +8,8 @@ fi
 apt update
 dpkg --add-architecture i386
 apt install software-properties-common -y
-wget https://dl.winehq.org/wine-builds/winehq.key && apt-key add winehq.key
+wget https://dl.winehq.org/wine-builds/winehq.key 
+apt-key add winehq.key
 rm winehq.key 
 echo 'deb https://dl.winehq.org/wine-builds/debian/ bullseye main'> /etc/apt/sources.list.d/winehq.list
 apt update
